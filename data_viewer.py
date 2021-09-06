c7b31536b9b69628056376e1307bb135c72ba905
CNPCNPCNP <89133671+CNPCNPCNP@users.noreply.github.com>
2021-09-06 17:00:32 +1000

    Add files via upload

---
+++ data_viewer.py
@@ -0,0 +1,58 @@
+"""
+Created on Thu Sep  2 10:32:05 2021
+
+@author: clayh
+"""
+import pandas as pd
+import datetime as dt
+import os
+
+#Global variable for the data folder path as we re-use it a lot, and it should
+#never change
+PATH = os.path.dirname(__file__) + '\\data'
+
+def plot_asx_summary_data(asx_df: pd.DataFrame) -> None:
+    """Takes a summary dataframe and plots ticker mentions over time. Filters 
+    out tickers that aren't mentioned at all"""
+    new_df = collapse_full_df(asx_df)
+    print(new_df)
+    new_df.plot(kind = 'line')
+    return None
+
+def collapse_full_df(asx_df: pd.DataFrame) -> pd.DataFrame:
+    """Takes a summary dataframe and deletes all the entries which have 0
+    mentions over the time period"""
+    for i in asx_df.columns:
+        if asx_df[i].all() == 0:
+            asx_df.drop(labels = i, axis = 1, inplace = True)
+    return asx_df
+
+def export_df_summary(asx_df_summary: pd.DataFrame, start_date, end_date) -> None:
+    start_date = start_date.strftime('%d_%m_%y')
+    end_date = end_date.strftime('%d_%m_%y')
+    title = 'summary_'
+    asx_df_summary.to_csv(os.path.join(PATH, title + start_date + '-' + 
+                                       end_date + '.csv'))
+    
+def export_counter(asx_df_dict: dict) -> None:
+    """Takes a pandas dataframe (ideally after the count of each ticker has 
+    been updated) and exports this to a CSV file to be viewed/edited further"""
+    for i in asx_df_dict.keys():
+        date = i.strftime('%d_%m_%y')
+        title = 'companies_counter_'
+        asx_df_dict[i].to_csv(os.path.join(PATH, title + date + '.csv'))
+    return None
+
+def file_name_list_checker() -> list:
+    """Checks the data folder for any asx summary files"""
+    summary_list = []
+    for file in os.listdir(PATH):
+        if file.startswith('summary'):
+            summary_list.append(file)
+    return summary_list
+
+def asx_summary_reader(files: list):
+    df_list = []
+    for i in files:
+        df_list.append = pd.read_csv(PATH + '\\' + i, index_col = 0)
+    return df_list
